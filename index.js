24344a059620325637fb79a58cb2d27e9db6db31
Praveen Toppo <impraveen.n91@gmail.com>
2023-02-02 22:23:47 +0530

    fixed url

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var inputEl=document.getElementById("input");
+const inputEl=document.getElementById("input");
+
+async function fetchAPI(word){
+    const url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    const result=await fetch(url).then((res)=>res.json())
+    console.log(result)
+}
 
 inputEl.addEventListener("keyup", (e)=>{
-    console.log(e.target.value);
+    if(e.target.value && e.key==="Enter")
+    {
+        fetchAPI(e.target.value)
+    }
 })
